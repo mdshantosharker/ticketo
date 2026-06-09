91ad70f644c0f0927c4b6f8a3c727b374770b94a
mdshantosharker <mdshantosharker@gmail.com>
2026-06-09 20:45:27 +0600

    added role base dashboard

--- ticketo-client/src/app/(dashboardLayout)/layout.jsx
+++ ticketo-client/src/app/(dashboardLayout)/layout.jsx
@@ -1,15 +1,13 @@
-import Logo from "@/components/Logo";
-import React from "react";
+"use client";
+import DashboardSidebar from "@/components/DashboardSidebar";
 
 const DashBoardLayout = ({ children }) => {
+
+
   return (
     <div className="min-h-screen flex bg-[#080c16]">
-      <aside className="w-64 h-screen border-r border-white">
-        <div className="px-6 py-5 border-b border-white">
-          <Logo />
-        </div>
-      </aside>
-      <div>{children}</div>
+      <DashboardSidebar />
+      <div className="px-6 py-10 max-w-7xl w-full mx-auto">{children}</div>
     </div>
   );
 };
